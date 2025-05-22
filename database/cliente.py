96f57567702016b688128731c4c08d2b10e87bf2
Cassiano Junior <cassiano.junior@hotmail.com>
2025-05-21 21:20:32 -0300

    create folder static, routes, database and templates

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id": 1, "nome": "Cassiano", "email": "cassiano@teste.com"},
+    {"id": 2, "nome": "Camila", "email": "camila@teste.com"},
+    {"id": 3, "nome": "Miguel", "email": "miguel@teste.com"},
+    {"id": 4, "nome": "Noah", "email": "noah@teste.com"},
+    {"id": 5, "nome": "Mia", "email": "mia@teste.com"},
+    {"id": 6, "nome": "Frajola", "email": "frajola@teste.com"},
+]
